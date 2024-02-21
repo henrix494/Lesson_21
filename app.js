36cdb756027d32ec889cbabf83465c589bad3e57
henrix494 <89205895+henrix494@users.noreply.github.com>
2024-02-21 10:29:36 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,23 @@
-const countryName = document.querySelectorAll("option");
 const capityalCity = document.querySelector("#capital");
-let countryNameValue = "tel aviv";
-capityalCity.textContent = countryNameValue;
-document.addEventListener("DOMContentLoaded", function () {
-  const capityalCity = document.querySelector("#capital");
-  const selectElement = document.querySelector("select");
+const selectElement = document.querySelector("select");
 
-  selectElement.addEventListener("change", (e) => {
-    let countryNameValue = e.target.value;
+capityalCity.textContent = "tel aviv";
 
-    switch (countryNameValue) {
-      case "isreal":
-        capityalCity.textContent = "tel aviv";
-        break;
-      case "italy":
-        capityalCity.textContent = "rome";
-        break;
-      case "brazil":
-        capityalCity.textContent = "brasilia";
-        break;
-      case "russia":
-        capityalCity.textContent = "moscow";
-        break;
-    }
-  });
+selectElement.addEventListener("change", (e) => {
+  let countryNameValue = e.target.value;
+
+  switch (countryNameValue) {
+    case "isreal":
+      capityalCity.textContent = "tel aviv";
+      break;
+    case "italy":
+      capityalCity.textContent = "rome";
+      break;
+    case "brazil":
+      capityalCity.textContent = "brasilia";
+      break;
+    case "russia":
+      capityalCity.textContent = "moscow";
+      break;
+  }
 });
